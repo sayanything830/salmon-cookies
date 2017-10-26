0e05db4effd7a6898847a014b2f5ae3472411c1e
Melanie Downing <sayanything830@gmail.com>
2017-10-26 09:00:45 -0700

    working on tables and cleaning up code

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
 'use strict';
+
+//use my constructor function
+// function Store(name, minCust, maxCust, avgCookieSales) {
+//   this.name = name; //grabs from parameters
+//   this.minCust = minCust; //grabs from parameters
+//   this.maxCust = maxCust; // grabs from parameters
+//   this.avgCookieSales = avgCookieSales; //grabs from parameters
+//   this.hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']; //hours store is open to sell cookies
+//   this.cookiesPerHour = []; //will be filled once method is called
+//   this.dailyTotal = 0; //will be filled once method is called
+//   this.randNum = [];
+// }
+// Store.prototype.randomCust = function () {
+//   for(var i = 0; i < this.hours.length; i++) { //runs loop for each hour store is open
+//     this.randNum.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)); //generates my random customers per hour
+//     // console.log(this.randNum);
+//   }
+//   this.generateCookieSales();
+//   return this.randNum;
+// };
+// Store.prototype.generateCookieSales = function () {
+//   // console.log('hi there', this);
+//   for(var j = 0; j < this.randNum.length; j ++) {
+//     this.cookiesPerHour.push(this.randNum[j] * this.avgCookieSales);
+//   }
+//   this.createDailyTotal();
+//   return this.cookiesPerHour;
+// };
+// Store.prototype.createDailyTotal = function() {
+//   console.log(this);
+//   for(var k = 0; k < this.hours.length; k ++) {
+//     this.dailyTotal += (Math.ceil(this.randNum[k] * this.avgCookieSales)); //tallies the total number of cookies as it runs through the loop
+//     // console.log(this.dailyTotal);
+//   }
+//   console.log('daily total', this.dailyTotal);
+//   return this.dailyTotal;
+// };
+//
+//
+//
+// firstAndPike.randomCust();
+// seaTac.randomCust();
+// seattleCenter.randomCust();
+// capitolHill.randomCust();
+// alki.randomCust();
+
+
+
+
 //starting over with a constructor function
 function Store(name, minCust, maxCust, avgCookieSales) {
   this.name = name; //grabs from parameters
@@ -8,19 +57,14 @@ function Store(name, minCust, maxCust, avgCookieSales) {
   this.hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']; //hours store is open to sell cookies
   this.cookiesPerHour = []; //will be filled once method is called
   this.dailyTotal = 0; //will be filled once method is called
+  this.randNum = [];
 }
 Store.prototype.randomCust = function () { //here is my method
-  var salesTally = 0;
-  var salesPerHour = [];
   for(var i = 0; i < this.hours.length; i++) { //runs loop for each hour store is open
-    var randNum = Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust); //generates my random customers per hour
-    salesPerHour.push(Math.ceil(randNum * this.avgCookieSales)); //multiplies random customers by average cookie sales given in parameter and moves each into my array
-    console.log(salesPerHour);
-    salesTally += (Math.ceil(randNum * this.avgCookieSales)); //tallies the total number of cookies as it runs through the loop
-    console.log(salesTally);
+    this.randNum = Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust); //generates my random customers per hour
+    this.cookiesPerHour.push(Math.ceil(this.randNum * this.avgCookieSales)); //multiplies random customers by average cookie sales given in parameter and moves each into my array
+    this.dailyTotal += (Math.ceil(this.randNum * this.avgCookieSales)); //tallies the total number of cookies as it runs through the loop
   }
-  this.cookiesPerHour = salesPerHour; //reassigns the above variable
-  this.dailyTotal = salesTally; //reassigns the above variable
 };
 
 //now I'm establishing the parameters per store
@@ -38,6 +82,7 @@ capitolHill.randomCust();
 alki.randomCust();
 
 //Now adding table to the domain
+var header = document.createElement('tr');
 var data = ['<td>Store Locations</td>'];
 var storeLocations = [];
 
@@ -53,23 +98,31 @@ for(var j = 0; j < storeLocations[0].hours.length; j ++) { //this pulls my hours
   console.log(data);
 }
 data = data + '<td>Daily Totals</td>';
-
-var header = document.createElement('tr');
 header.innerHTML = data;
 document.getElementById('table_head').appendChild(header);
 
+function createTable (store) {
+  var tableBody = document.createElement('tr');
+  var tableData = ['<td>' + store.name + '</td>'];
+  for(var k = 0; k < store.cookiesPerHour.length; k++) {
+    tableData = tableData + '<td>' + store.cookiesPerHour[k] + '</td>';
+  }
+  tableData = tableData + '<td>' + store.dailyTotal + '</td>';
+  tableBody.innerHTML = tableData;
+  document.getElementById('table_content').appendChild(tableBody);
+}
+createTable();
 
-var dataContent = [];
-for(var k = 0; k < storeLocations[0].hours.length; k ++) {
-  dataContent.push(
-    '<td>' + storeLocations[k].cookiesPerHour + '</td>'
-  );
-  console.log(dataContent);
-};
-
-var tableBody = document.createElement('tr');
-tableBody.innerHTML = dataContent;
-document.getElementById('table_content').appendChild(tableBody);
+// for(var k = 0; k < storeLocations[0].hours.length; k ++) {
+//   dataContent.push(
+//     '<td>' + Store.cookiesPerHour + '</td>'
+//   );
+//   console.log(dataContent);
+// };
+//
+//
+// tableBody.innerHTML = dataContent;
+// document.getElementById('table_content').appendChild(tableBody);
 
 // var newRow = document.createElement('tr');
 // newRow.innerHTML = dataContent.join('');
